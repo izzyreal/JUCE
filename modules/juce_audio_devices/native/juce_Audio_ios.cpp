23228027c3b97ac1976aac13aeb2f71e6a60ac70
Izmar Verhage <izmaelverhage@gmail.com>
2022-10-14 20:14:50 +0200

    Fix two iOS audio issues
    By commenting out AVAudioSessionCategoryOptionAllowBluetooth we only
    disable bluetooth input, which is desirable, because the audio output
    stream of a bidirectional bluetooth stream is 16KHz sample rate. By
    not passing AVAudioSessionCategoryOptionAllowBluetooth, we get good
    sample rates with bluetooth devices.
    
    The AVAudioSessionCategoryOptionOverrideMutedMicrophoneInterruption
    patch is related a privacy feature introduced in iOS 14.5, where the
    microphone is muted when closing the iPad's Smart Folio (magnetic)
    cover. If we don't pass
    AVAudioSessionCategoryOptionOverrideMutedMicrophoneInterruption,
    the audio stream will be interrupted when closing the cover. In
    JUCE this is not handled correctly, and the stream is bidirectionally
    interrupted and never recovers during the lifetime of the app. An
    app restart is needed to get processBlock calls again.

--- modules/juce_audio_devices/native/juce_Audio_ios.cpp
+++ modules/juce_audio_devices/native/juce_Audio_ios.cpp
@@ -372,9 +372,15 @@ struct iOSAudioIODevice::Pimpl final : public AsyncUpdater
         if (category == AVAudioSessionCategoryPlayAndRecord)
         {
             options |= AVAudioSessionCategoryOptionDefaultToSpeaker
-                     | AVAudioSessionCategoryOptionAllowBluetooth
-                     | AVAudioSessionCategoryOptionAllowAirPlay
-                     | AVAudioSessionCategoryOptionAllowBluetoothA2DP;
+                        | AVAudioSessionCategoryOptionAllowAirPlay
+                        | AVAudioSessionCategoryOptionAllowBluetoothA2DP;
+                      /*| AVAudioSessionCategoryOptionAllowBluetooth */;
+
+            if (@available (iOS 14.5, *))
+                options |= AVAudioSessionCategoryOptionOverrideMutedMicrophoneInterruption;
+
+            if (@available (iOS 10.0, *))
+                options |= AVAudioSessionCategoryOptionAllowBluetoothA2DP;
         }
 
         JUCE_NSERROR_CHECK ([[AVAudioSession sharedInstance] setCategory: category
